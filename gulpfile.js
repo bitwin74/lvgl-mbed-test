1b94d6a187070672058e20ceef5e2703792c645c
Jonathan Dinh <jdinh1@csub.edu>
2019-01-20 13:45:36 -0800

    Formatting

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var format = require('gulp-clang-format');
+
+var cppFiles = [
+	'src/*.cpp',
+	'src/*.c',
+	'src/*.h',
+
+];
+
+gulp.task('check-format', function() {
+	return gulp.src(cppFiles)
+		.pipe(format.checkFormat('file', undefined, {verbose: true, fail: true}));
+});
+
+gulp.task('format', function() {
+	// The base option ensures the glob doesn't strip prefixes
+	return gulp.src(cppFiles, {base: '.'})
+		.pipe(format.format('file', undefined, {verbose: true, fail: true}))
+		.pipe(gulp.dest('.'));
+});
+
--- src/lv_ex_conf.h
+++ src/lv_ex_conf.h
@@ -14,20 +14,19 @@
 /*******************
  * GENERAL SETTING
  *******************/
-#define LV_EX_PRINTF       0       /*Enable printf-ing data*/
-#define LV_EX_KEYBOARD     0       /*Add PC keyboard support to some examples (`lv_drivers` repository is required)*/
-#define LV_EX_MOUSEWHEEL   0       /*Add 'encoder' (mouse wheel) support to some examples (`lv_drivers` repository is required)*/
+#define LV_EX_PRINTF 0     /*Enable printf-ing data*/
+#define LV_EX_KEYBOARD 0   /*Add PC keyboard support to some examples (`lv_drivers` repository is required)*/
+#define LV_EX_MOUSEWHEEL 0 /*Add 'encoder' (mouse wheel) support to some examples (`lv_drivers` repository is required)*/
 
 /*******************
  *   TEST USAGE
  *******************/
-#define USE_LV_TESTS        0
+#define USE_LV_TESTS 0
 
 /*******************
  * TUTORIAL USAGE
  *******************/
-#define USE_LV_TUTORIALS   0
-
+#define USE_LV_TUTORIALS 0
 
 /*********************
  * APPLICATION USAGE
@@ -35,26 +34,25 @@
 
 /* Test the graphical performance of your MCU
  * with different settings*/
-#define USE_LV_BENCHMARK   0
+#define USE_LV_BENCHMARK 0
 
 /*A demo application with Keyboard, Text area, List and Chart
  * placed on Tab view */
-#define USE_LV_DEMO        0
+#define USE_LV_DEMO 0
 #if USE_LV_DEMO
-#define LV_DEMO_WALLPAPER  1    /*Create a wallpaper too*/
-#define LV_DEMO_SLIDE_SHOW 0    /*Automatically switch between tabs*/
+#define LV_DEMO_WALLPAPER 1  /*Create a wallpaper too*/
+#define LV_DEMO_SLIDE_SHOW 0 /*Automatically switch between tabs*/
 #endif
 
 /*MCU and memory usage monitoring*/
-#define USE_LV_SYSMON      0
+#define USE_LV_SYSMON 0
 
 /*A terminal to display received characters*/
-#define USE_LV_TERMINAL    0
+#define USE_LV_TERMINAL 0
 
 /*Touch pad calibration with 4 points*/
-#define USE_LV_TPCAL       0
+#define USE_LV_TPCAL 0
 
 #endif /*LV_EX_CONF_H*/
 
 #endif /*End of "Content enable"*/
-
