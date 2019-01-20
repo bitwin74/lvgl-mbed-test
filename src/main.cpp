efaa91cc598af0759d31862fc4a594180db62583
Jonathan Dinh <jdinh1@csub.edu>
2019-01-20 13:34:27 -0800

    Cleaning up `hello world`

--- src/main.cpp
+++ src/main.cpp
@@ -34,17 +34,7 @@ int main() {
 
     lv_disp_drv_register(&disp_drv);
 
-
-	/*Create a Label on the currently active screen*/
-    lv_obj_t * label1 =  lv_label_create(lv_scr_act(), NULL);
-
-    /*Modify the Label's text*/
-    lv_label_set_text(label1, "Hello world!");
-
-    /* Align the Label to the center
-     * NULL means align on parent (which is the screen now)
-     * 0, 0 at the end means an x, y offset after alignment*/
-    lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
+	draw_hello_world();
 
     while(1) {
 		lv_task_handler();
